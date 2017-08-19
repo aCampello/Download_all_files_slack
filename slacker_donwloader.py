c2731e74edf21e20dc44df86aab2cec3c8004d6e
aCampello <antonio.campello@icloud.com>
2017-08-19 23:28:32 +0100

    Changes in slacker_downloader"

--- slacker_donwloader.py
+++ slacker_donwloader.py
@@ -1,5 +1,6 @@
 import slacker
 import sys
+import os
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
@@ -16,6 +17,9 @@ if __name__ == "__main__":
 		print('Error connecting to slack.')
 		sys.exit()
 
+	if not os.path.exists('download'):
+		os.mkdir('download')
+
 	for f in file_list:
 		file_to_download = f.get('url_private_download')
 		if file_to_download:
@@ -24,8 +28,8 @@ if __name__ == "__main__":
 				file_name = file_to_download.split('/')[-1]
 				
 				print('Downloading {}'.format(file_name))
-				
-				with open(file_name,'wb') as file_save:
+
+				with open('download/'+file_name,'wb') as file_save:
                 			file_save.write(slack_file.content)
 
 
